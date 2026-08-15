1d81a06b39b6b6bb5028bfa28819df7613fef782
Chinmay Bansal <chinmay8521@gmail.com>
2026-08-15 15:41:11 +0530

    feat: add fish completion

---
+++ completions/devclean.fish
@@ -0,0 +1,10 @@
+complete -c devclean -n '__fish_use_subcommand' -a 'scan clean stats analyze'
+complete -c devclean -l help
+complete -c devclean -s V -l version
+complete -c devclean -l json
+complete -c devclean -l active-only
+complete -c devclean -l min-size
+complete -c devclean -l max-size
+complete -c devclean -l dry-run
+complete -c devclean -l sort
+complete -c devclean -l reverse

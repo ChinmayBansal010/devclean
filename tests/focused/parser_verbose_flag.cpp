cc8122ebe134455ea44a2c2f7e949c32fd954a5d
Chinmay Bansal <chinmay8521@gmail.com>
2026-08-23 14:54:01 +0530

    test: parse verbose output mode

---
+++ tests/focused/parser_verbose_flag.cpp
@@ -0,0 +1,3 @@
+#include "core/ArgumentParser.hpp"
+#include <cassert>
+int main(){char a0[]="devclean",a1[]="scan",a2[]="--verbose";char* v[]={a0,a1,a2};const auto a=ArgumentParser::parse(3,v);assert(a.verbose);return 0;}

46163f75ce268ea9d5fd60cdaed54fd825210a09
Chinmay Bansal <chinmay8521@gmail.com>
2026-08-23 14:54:29 +0530

    test: parse minimum size units

---
+++ tests/focused/parser_min_size_kb.cpp
@@ -0,0 +1,3 @@
+#include "core/ArgumentParser.hpp"
+#include <cassert>
+int main(){char a0[]="devclean",a1[]="scan",a2[]="--min-size",a3[]="2KB";char* v[]={a0,a1,a2,a3};const auto a=ArgumentParser::parse(4,v);assert(a.minSizeBytes==2048);return 0;}

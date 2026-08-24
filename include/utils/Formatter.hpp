15876ea233a5c6fe140afebf3d043a16018b173e
Chinmay Bansal <chinmay8521@gmail.com>
2026-08-24 21:26:40 +0530

    feat: expose grouped count formatting

--- include/utils/Formatter.hpp
+++ include/utils/Formatter.hpp
@@ -9,5 +9,6 @@ class Formatter
 public:
     static std::string formatBytes(uint64_t bytes);
     static std::string formatSignedBytes(int64_t bytes);
+    static std::string formatCount(uint64_t count);
     static std::string formatPath(const std::filesystem::path& path);
 };

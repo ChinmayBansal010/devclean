9039e7d7c47895408d3c4cd6540e1821a40c424e
Chinmay Bansal <chinmay8521@gmail.com>
2026-08-24 21:30:01 +0530

    feat: allow application interrupt state reset

--- include/core/Application.hpp
+++ include/core/Application.hpp
@@ -7,6 +7,7 @@ class Application
 public:
     int run(int argc, char* argv[]);
     static bool isInterrupted();
+    static void resetInterrupt();
 
 private:
     static volatile sig_atomic_t interrupted;

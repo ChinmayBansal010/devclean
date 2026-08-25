f5ee418b64446bae5fa487eed5e341f0bfc8cdb8
Chinmay Bansal <chinmay8521@gmail.com>
2026-08-25 16:34:04 +0530

    feat: expose case insensitive suffix matching

--- include/utils/StringUtils.hpp
+++ include/utils/StringUtils.hpp
@@ -7,6 +7,7 @@ namespace StringUtils {
 std::string lower(std::string_view value);
 bool startsWith(std::string_view value, std::string_view prefix);
 bool endsWith(std::string_view value, std::string_view suffix);
+bool endsWithIgnoreCase(std::string_view value, std::string_view suffix);
 bool equalsIgnoreCase(std::string_view lhs, std::string_view rhs);
 std::string trim(std::string_view value);
 }

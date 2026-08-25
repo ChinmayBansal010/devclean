1375e98d826374c335b140dcddf57a7a9c3144aa
Chinmay Bansal <chinmay8521@gmail.com>
2026-08-25 16:30:52 +0530

    feat: expose whitespace trimming utility

--- include/utils/StringUtils.hpp
+++ include/utils/StringUtils.hpp
@@ -7,4 +7,5 @@ namespace StringUtils {
 std::string lower(std::string_view value);
 bool startsWith(std::string_view value, std::string_view prefix);
 bool equalsIgnoreCase(std::string_view lhs, std::string_view rhs);
+std::string trim(std::string_view value);
 }

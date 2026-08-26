c9f77159b2c47405668848d02245a4e54869b167
Chinmay Bansal <chinmay8521@gmail.com>
2026-08-26 19:17:38 +0530

    test: verify reverse scan ordering flag

--- tests/focused/parser_reverse_flag.cpp
+++ tests/focused/parser_reverse_flag.cpp
@@ -1,3 +1,3 @@
 #include "core/ArgumentParser.hpp"
 #include <cassert>
-int main(){char a0[]="devclean",a1[]="scan",a2[]="--reverse";char* v[]={a0,a1,a2};const auto a=ArgumentParser::parse(3,v);assert(a.reverse);return 0;}
+int main(){char a0[]="devclean";char a1[]="scan";char a2[]="--reverse";char* argv[]={a0,a1,a2};const auto args=ArgumentParser::parse(3,argv);assert(args.reverse);return 0;}

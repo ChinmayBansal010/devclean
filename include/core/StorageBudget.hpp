3056fdf8ac80fc512ada0ccc5b3ec18cd5f9a2e7
Chinmay Bansal <chinmay8521@gmail.com>
2026-08-27 20:41:28 +0530

    feat: expose storage budget reclaim coverage

--- include/core/StorageBudget.hpp
+++ include/core/StorageBudget.hpp
@@ -35,3 +35,4 @@ std::vector<StorageBudgetItem> rankBudgetCandidates(std::vector<StorageBudgetIte
 double budgetUsageRatio(const StorageBudget& budget);
 double budgetProjectedUsageRatio(const StorageBudget& budget);
 uint64_t budgetRequiredReclaim(const StorageBudget& budget);
+double budgetReclaimCoverage(const StorageBudget& budget);
